83ad803b338a53958ce07c03f81b61ab9ed2688d
doakcovington <doakcovington@protonmail.com>
2020-08-28 14:45:00 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@
 doak = User.create(username: 'doak', email: 'doak@email.com', password: '123')
 heidi = User.create(username: 'heidi', email: 'heidi@email.com', password: '123')
 
-post_1 = Post.create(date: "8/27/2020", location:"Bellingham, WA", image:"test.com", user_id: 1)
+Post.create(date: "8/27/2020", location:"Bellingham, WA", sends: 2, image:"test.com", user_id: 6, category_id: 3)
 
-comment_1 = Comment.create(body: "Thats a big send!", user_id: 2, post_id: 1)
+comment_1 = Comment.create(body: "Thats a big send!", user_id: 7, post_id: 3)
+
+category_1 = Category.create(name: "Downhill")
