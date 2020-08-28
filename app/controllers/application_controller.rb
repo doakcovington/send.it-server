f794a9d2c10e53436336c0e23661855ef3245a60
doakcovington <doakcovington@protonmail.com>
2020-08-28 14:48:41 -0700

    Added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    helpers do
+        def current_user
+            @user = User.find_by(id: user_id)  
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
 end
