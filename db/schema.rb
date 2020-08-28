e0d192c6ae10e67fce0faca33a71ea013a6e0c1e
doakcovington <doakcovington@protonmail.com>
2020-08-27 20:24:00 -0700

    Migration and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_022126) do
+ActiveRecord::Schema.define(version: 2020_08_28_014545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "date"
+    t.string "location"
+    t.string "image"
+    t.string "sends"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
