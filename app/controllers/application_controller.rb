131bdece6f97f090a056e61e8b67736cb8523953
doakcovington <doakcovington@protonmail.com>
2020-08-28 15:18:46 -0700

    Added cors initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_user
-            @user = User.find_by(id: user_id)  
-        end
 
-        def logged_in?
-            !!current_user
-        end
+    def current_user
+        @user = User.find_by(id: user_id)  
+    end
+
+    def logged_in?
+        !!current_user
     end
 end
