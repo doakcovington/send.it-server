5125392f3d7d0f57f8adef598897e8e914a2eb93
doakcovington <doakcovington@protonmail.com>
2020-08-27 20:26:01 -0700

    index and show routes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::PostsController < ApplicationController
+      # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
 end
