f1f079b400e83366a30e6d3ef1d5dc423763d60f
doakcovington <doakcovington@protonmail.com>
2020-08-31 19:01:13 -0700

    User can log in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,4 +14,14 @@ class Api::V1::SessionsController < ApplicationController
     def destroy
 
     end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user 
+        else
+            render json: {
+                error: "Not logged in"
+            }
+        end
+    end
 end
