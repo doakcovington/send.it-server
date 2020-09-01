9e170f19e16f12cbbf49a4e82d1e79e7b6a92d5b
doakcovington <doakcovington@protonmail.com>
2020-09-01 14:57:43 -0700

    Added serializer to get_current_user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::SessionsController < ApplicationController
 
     def get_current_user
         if logged_in?
-            render json: current_user 
+            render json: UserSerializer.new(current_user) #only shows id, type, username, email
         else
             render json: {
                 error: "Not logged in"
